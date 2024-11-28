fee670fa622621b019696dbf533defcda2e71b8b
Alex Rosegrid <rosegrid58@gmail.com>
2024-11-28 22:11:33 +0530

    Add emacs' early-init file

---
+++ .dotfiles/.emacs.d/early-init.el
@@ -0,0 +1,15 @@
+(setq-default truncate-lines t)
+(setq-default blink-cursor-mode nil)
+(setq-default inhibit-startup-screen t)
+
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+
+(setq package-quickstart t)
+
+(setq gc-cons-threshold (* 64 1000 1000))
+(setq read-process-output-max (* 1024 1024))
+
+(setq scroll-conservatively 101)
+(setq echo-keystrokes .1)
