d02e2c56755a375e1ec174bbab381c2b9be0c6b8
Alex Rosegrid <rosegrid58@gmail.com>
2024-11-29 10:04:51 +0530

    Add Org mode settings

--- .dotfiles/.emacs.d/init.el
+++ .dotfiles/.emacs.d/init.el
@@ -5,8 +5,47 @@
 (global-set key "\C-xf" 'recentf-open-files)
 
 (electric-pair-mode)
+(setq electric-pair-inhibit-predicate 'electric-pair-conservative-inhibit)
 
 (setq-default global-display-line-numbers-mode t)
 (setq-default display-line-numbers 'relative)
 
 (auto-revert-mode 1)
+
+(add-hook 'org-mode-hook
+          (lambda() (setq jit-lock-defer-time 0.15)))
+
+(add-hook 'org-mode-hook
+          (lambda()
+            (setq org-latex-src-block-backend 'listing
+                  org-lagex-listings-options  '(("numbers" "left")
+                                                ("breaklines" "true")
+                                                ("upquote" "true")
+                                                ("autogobble" "true")
+                                                ("showstringspaces" "false")
+                                                ("basicstyle" "\\ttfamily")))))
+
+(add-hook 'org-mode-hook
+          (lambda()
+            (setq org-preview-latex-default-process 'dvipng
+                  org-startup-with-latex-preview     t)))
+
+(add-hook 'org-mode-hook
+          (lambda()
+            (org-babel-do-load-languages
+             'org-babel-load-languages '((emacs-lisp . t)
+                                         (C . t)
+                                         (shell . t)
+                                         (lua . t))))
+
+(add-hook 'org-mode-hook
+          (lambda()
+            (display-line-numbers-mode -1)
+            (setq org-startup-indented t)))
+
+(add-hook 'org-mode-hook
+          (lambda()
+            (setq-local electric-pair-inhibit-predicate
+                        `(lambda(c)
+                           (if (char-equal c ?<) t
+                             (,electric-pair-inhibit-predicate c))))))
