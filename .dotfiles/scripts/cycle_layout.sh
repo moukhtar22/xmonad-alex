9a9e75aa71192c69aff078532d92fb0d148fcc28
Alex Rosegrid <rosegrid58@gmail.com>
2025-01-06 10:45:37 +0530

    Remove DVORAK from layout cycle

--- .dotfiles/scripts/cycle_layout.sh
+++ .dotfiles/scripts/cycle_layout.sh
@@ -6,10 +6,6 @@ STATUS_FILE=/tmp/current-variant
 
 CURRENT_VARIANT="$(cat $STATUS_FILE)"
 if [[ $CURRENT_VARIANT = "qwerty" ]]; then
-    notify-send -t 300 "Switching to DVORAK"
-    setxkbmap -variant dvorak
-    echo "dvorak" > $STATUS_FILE
-elif [[ $CURRENT_VARIANT = "dvorak" ]]; then
     notify-send -t 300 "Switching to colemak Mod-DH"
     setxkbmap -variant colemak_dh
     echo "colemak-dh" > $STATUS_FILE
