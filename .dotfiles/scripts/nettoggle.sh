3fe2fd99d9112170e5f29935a3eb41ab2b3b840a
Alex Rosegrid <rosegrid58@gmail.com>
2025-01-08 21:18:27 +0530

    Update nettoggle script

--- .dotfiles/scripts/nettoggle.sh
+++ .dotfiles/scripts/nettoggle.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+try_connecting () {
+    for network in $(cat ~/.networks); do
+        iwctl station wlan0 connect $network&& notify-send "Connect to $network"&& exit 0
+    done
+}
+
 if [[ $(iw dev wlan0 info| grep "ssid" -c) -eq 0 ]]; then
     iwctl station wlan0 scan on
     if [[ -f ~/.networks ]]; then
-        for network in $(cat ~/.networks); do
-            iwctl station wlan0 connect $network&& notify-send "Connect to $network"&& exit 0
-        done
+        for i in {1..2}; do try_connecting; done
     else
         notify-send "Required: a ~/.networks file, as a list of known networks (newline separated)"
         alacritty -e iwctl
