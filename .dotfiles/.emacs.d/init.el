bb6388ed6f28a152f506e1c32ee9eb030cdaf9f9
Alex Rosegrid <rosegrid58@gmail.com>
2025-02-21 21:26:12 +0530

    Add projectile to Emacs

--- .dotfiles/.emacs.d/init.el
+++ .dotfiles/.emacs.d/init.el
@@ -107,6 +107,10 @@
   :ensure t
   :defer  t)
 
+(use-package projectile
+  :ensure t
+  :bind ("C-c p" . projectile-command-map))
+
 (use-package rainbow-delimiters
   :ensure t
   :hook prog-mode)
@@ -128,10 +132,6 @@
   (doom-themes-org-config))
 
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  '(default ((t (:family "JetBrains Mono" :foundry "JB" :slant normal :weight regular :height 143 :width normal)))))
 
 (use-package doom-modeline
@@ -243,15 +243,3 @@
         (c++-mode  . c++-ts-mode)
         (bash-mode . bash-ts-mode)
         (java-mode . java-ts-mode)))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(all-the-icons corfu doom-modeline doom-themes elpy evil-collection
-		   haskell-mode lsp-haskell lsp-java lsp-pyright
-		   lsp-ui lua-mode magit marginalia olivetti
-		   org-bullets pdf-tools portage-modes
-		   rainbow-delimiters rainbow-identifiers rainbow-mode
-		   vertico vterm yasnippet-snippets)))
