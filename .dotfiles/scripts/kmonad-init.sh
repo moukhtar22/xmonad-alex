5539078a5eaa785dd13f949263b5f0f34be36053
Alex Rosegrid <rosegrid58@gmail.com>
2025-03-08 09:34:28 +0530

    Fix symlink overwrite in KMonad init script

--- .dotfiles/scripts/kmonad-init.sh
+++ .dotfiles/scripts/kmonad-init.sh
@@ -14,7 +14,7 @@ fi
 if [[ -n "$KBD_DEVICE" ]]; then
     echo "Using $KBD_DEVICE as keyboard device (making a copy of the previous config)"
     cp ~/.config/kmonad/kmonad-alex.kbd ~/.config/kmonad/kmonad-alex.kbd.old
-    sed -i "s|/dev/input/by-id/[^\"]*|$KBD_DEVICE|" ~/.config/kmonad/kmonad-alex.kbd
+    sed -i "s|/dev/input/by-id/[^\"]*|$KBD_DEVICE|" ~/xmonad-alex/.dotfiles/.config/kmonad/kmonad-alex.kbd
 else
     echo "No external keyboard device was found; aborting"
     echo "You might want to try specifying a device name manually"
