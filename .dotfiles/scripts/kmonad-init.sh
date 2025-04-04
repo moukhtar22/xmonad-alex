2dde840430165e214a13f04ee806177ad6501057
Alex Rosegrid <rosegrid58@gmail.com>
2025-04-04 22:35:10 +0530

    Add internal keyboacd detect clause in the KMonad init script

--- .dotfiles/scripts/kmonad-init.sh
+++ .dotfiles/scripts/kmonad-init.sh
@@ -1,24 +1,65 @@
-#!/bin/bash
+#!/bin/zsh
 
 if [[ -n "$(pgrep 'kmonad$')" ]]; then
     killall kmonad
 fi
 
+# Canonicalize the args
+GETOPTS_OUT=$(getopt -o i -l use-internal -n "kmonad-init.sh" -- "$@")
+if [[ $? != 0 ]]; then
+	echo "getopts gave error; aborting" >&2
+	exit 1
+fi
+eval set -- "$GETOPTS_OUT"
+
+# Setup Variables
+USE_INTERNAL=0    # Do not look for external keyboards if asked for internal
+
+while true; do
+	case "$1" in
+		-i | --use-internal )
+			USE_INTERNAL=1
+			shift ;;
+		* ) shift; break ;;
+	esac
+done
 
 # Setup keyboard device
-KBD_DEVICE="$1"
-if [[ -z "$KBD_DEVICE" ]]; then
-    KBD_DEVICE="$(find /dev/input/by-id/ -name '*kbd')"
-fi
 
-if [[ -n "$KBD_DEVICE" ]]; then
-    echo "Using $KBD_DEVICE as keyboard device (making a copy of the previous config)"
-    cp ~/.config/kmonad/kmonad-alex.kbd ~/.config/kmonad/kmonad-alex.kbd.old
-    sed -i "s|/dev/input/by-id/[^\"]*|$KBD_DEVICE|" ~/xmonad-alex/.dotfiles/.config/kmonad/kmonad-alex.kbd
+if [[ $USE_INTERNAL -eq 0 ]]; then
+	KBD_DEVICE="$1"
+	if [[ -z "$KBD_DEVICE" ]]; then
+		echo "No device file was specified; will look for an input device: *-kbd"
+		KBD_DEVICE="$(find /dev/input/by-id/ -name '*kbd')"
+	fi
+	# If a keyboard device was specified, or was found, start KMonad using it
+	if [[ -n "$KBD_DEVICE" ]]; then
+		echo "Using $KBD_DEVICE as keyboard device (making a copy of the previous config)"
+		cp ~/.config/kmonad/kmonad-alex.kbd ~/.config/kmonad/kmonad-alex.kbd.old
+		sed -i "s|/dev/input/by-id/[^\"]*|$KBD_DEVICE|" ~/xmonad-alex/.dotfiles/.config/kmonad/kmonad-alex.kbd
+		(~/.local/bin/kmonad ~/.config/kmonad/kmonad-alex.kbd &)
+	else
+		echo "No extenal keyboard device was found; aborting"
+		echo "You might want to try the --use-internal flag"
+	fi
 else
-    echo "No external keyboard device was found; aborting"
-    echo "You might want to try specifying a device name manually"
-    exit 1
+    echo "Checking for an internal keyboard"
+	# Working under the assumption that a laptop's internal keyboard is called "AT Translated Set 2"
+	INTERNAL_KBD_DEVICE="$(journalctl -b -1| grep -i 'at translated set 2'| grep -i 'watching.*on'| sed -E 's|.*(/dev/.*) \(.*\)|\1|')"
+	if [[ -n "$INTERNAL_KBD_DEVICE" ]]; then
+		echo "$INTERNAL_KBD_DEVICE will be used"
+		cp ~/.config/kmonad/kmonad-alex.kbd ~/.config/kmonad/kmonad-alex-internal.kbd
+		sed -i "s|/dev/input/by-id/[^\"]*|$INTERNAL_KBD_DEVICE|" ~/.config/kmonad/kmonad-alex-internal.kbd
+		sed -i "s/KMonad Alex/Kmonad Alex Laptop Internal Keyboard/" ~/.config/kmonad/kmonad-alex-internal.kbd
+		# Patch the file to fix some keybindings
+		PATCH_FILE="$HOME/scripts/internal.patch"
+		cat $PATCH_FILE
+		echo "Should these changes be made?[Ctrl-c to abort, ENTER to continue]"&& read
+		patch -b -i $PATCH_FILE ~/.config/kmonad/kmonad-alex-internal.kbd
+		(~/.local/bin/kmonad ~/.config/kmonad/kmonad-alex-internal.kbd &)
+	else
+		echo "Nothing appropriate was found"
+		echo "You might want to try specifying a device name manually"
+		exit 1
+	fi
 fi
-
-(~/.local/bin/kmonad ~/.config/kmonad/kmonad-alex.kbd &)
