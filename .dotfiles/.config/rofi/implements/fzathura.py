213f3f6a47f0b08beaa976bd6b6fe9164088e61b
Alex Rosegrid <rosegrid58@gmail.com>
2025-04-06 09:44:07 +0530

    Add FZathura (py) command line argument parsing

--- .dotfiles/.config/rofi/implements/fzathura.py
+++ .dotfiles/.config/rofi/implements/fzathura.py
@@ -1,15 +1,29 @@
 #!/usr/bin/python
 
 import os
+import argparse
+
+
+class FZathuraParser(argparse.ArgumentParser):
+    def __init__(self):
+        super().__init__(
+            prog="FZathura",
+            description="A simple frontend for Zathura"
+        )
+        self.add_argument('-r', "--resume", action="store_true")
+        self.add_argument('-m', "--menu", action="store_true")
+        self.add_argument('-n', "--no-save-recent", action="store_true")
+        self.add_argument('-p', "--pdf")
+
+
+def set_up_root_dir() -> None:
+    HOME: str | None = os.getenv("HOME")
+    if HOME is None:
+        print("Failed to fetch envvars")
+        exit()
+    os.chdir(HOME + "/Documents")
 
 def get_all_pdfs_in_documents() -> list[str]:
-    def set_up_root_dir() -> None:
-        HOME: str | None = os.getenv("HOME")
-        if HOME is None:
-            print("Failed to fetch envvars")
-            exit()
-        os.chdir(HOME + "/Documents")
-    set_up_root_dir()
     all_pdfs: list[str] = []
     for path_name, _, files in os.walk("."):
         pdf_files: list[str] = [ child for child in files if child.endswith(".pdf") ]
@@ -17,3 +31,7 @@ def get_all_pdfs_in_documents() -> list[str]:
         for pdf_file in pdf_files:
             all_pdfs.append(os.path.join(rel_path_name, pdf_file))
     return all_pdfs
+
+
+set_up_root_dir()
+args = FZathuraParser().parse_args()
