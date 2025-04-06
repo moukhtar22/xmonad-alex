43152e08ab4b7902b09ae1a077752131f34811fa
Alex Rosegrid <rosegrid58@gmail.com>
2025-04-06 10:16:27 +0530

    Implement rofi selection grabbing for FZathura (py)

--- .dotfiles/.config/rofi/implements/fzathura.py
+++ .dotfiles/.config/rofi/implements/fzathura.py
@@ -2,6 +2,7 @@
 
 import os
 import argparse
+import subprocess
 
 
 class FZathuraParser(argparse.ArgumentParser):
@@ -16,12 +17,13 @@ class FZathuraParser(argparse.ArgumentParser):
         self.add_argument('-p', "--pdf")
 
 
-def set_up_root_dir() -> None:
+def set_up_root_dir() -> str:
     HOME: str | None = os.getenv("HOME")
     if HOME is None:
         print("Failed to fetch envvars")
         exit()
     os.chdir(HOME + "/Documents")
+    return HOME
 
 def get_all_pdfs_in_documents() -> list[str]:
     all_pdfs: list[str] = []
@@ -32,6 +34,17 @@ def get_all_pdfs_in_documents() -> list[str]:
             all_pdfs.append(os.path.join(rel_path_name, pdf_file))
     return all_pdfs
 
+def get_rofi_menu_selection(home_dir: str) -> str:
+    rofi_dmenu_proc = subprocess.Popen(
+        ["rofi", "-theme", f"{home_dir}/.config/rofi/themes/dmenu.rasi",
+        "-i", "-dmenu"],
+        stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+        text=True
+    )
+    all_pdfs: str = "\n".join(get_all_pdfs_in_documents())
+    output, _ = rofi_dmenu_proc.communicate(input=all_pdfs)
+    return output.replace('\n', '')
 
-set_up_root_dir()
+
+HOME:str = set_up_root_dir()
 args = FZathuraParser().parse_args()
