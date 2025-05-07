4bbbfcc450af6c6c939053935e0fad5eca4a1076
Alex Rosegrid <rosegrid58@gmail.com>
2025-05-07 23:01:55 +0530

    Fix fzathura --pdf switch (file path settings)

--- .dotfiles/.config/rofi/implements/fzathura.py
+++ .dotfiles/.config/rofi/implements/fzathura.py
@@ -18,21 +18,22 @@ class FZathuraParser(argparse.ArgumentParser):
         self.add_argument('-p', "--pdf")
 
 
-def set_up_root_dir() -> str:
+def get_home_dir() -> str:
     HOME: str | None = os.getenv("HOME")
     if HOME is None:
         print("Failed to fetch envvars")
         exit()
-    os.chdir(HOME + "/Documents")
     return HOME
 
-def get_all_pdfs_in_documents() -> list[str]:
+def get_all_pdfs_in_documents(home_dir: str) -> list[str]:
     all_pdfs: list[str] = []
+    os.chdir(f"{home_dir}/Documents")
     for path_name, _, files in os.walk("."):
         pdf_files: list[str] = [ child for child in files if child.endswith(".pdf") ]
         rel_path_name: str = path_name.replace("./", "")
         for pdf_file in pdf_files:
             all_pdfs.append(os.path.join(rel_path_name, pdf_file))
+    os.chdir(f"{home_dir}")
     return all_pdfs
 
 def get_rofi_menu_selection(home_dir: str) -> str:
@@ -42,8 +43,9 @@ def get_rofi_menu_selection(home_dir: str) -> str:
         stdin=subprocess.PIPE, stdout=subprocess.PIPE,
         text=True
     )
-    all_pdfs: str = "\n".join(get_all_pdfs_in_documents())
+    all_pdfs: str = "\n".join(get_all_pdfs_in_documents(home_dir))
     output, _ = rofi_dmenu_proc.communicate(input=all_pdfs)
+    output = f"{home_dir}/Documents/{output}"
     return output.replace('\n', '')
 
 def zathura_open_pdf(pdf_file: str, log_file:str, save_to_recents: bool = True) -> None:
@@ -53,7 +55,7 @@ def zathura_open_pdf(pdf_file: str, log_file:str, save_to_recents: bool = True) 
     subprocess.run(f"zathura --fork '{pdf_file}'", shell=True)
 
 
-HOME: Final[str] = set_up_root_dir()
+HOME: Final[str] = get_home_dir()
 args = FZathuraParser().parse_args()
 LOG_FILE: Final[str] = f"{HOME}/.config/zathura/recents.log"
 
