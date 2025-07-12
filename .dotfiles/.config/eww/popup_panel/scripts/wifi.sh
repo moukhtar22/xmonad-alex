852b37d4a69529926e94d790cafca84b1093a117
Alex Rosegrid <rosegrid58@gmail.com>
2025-07-12 20:11:21 +0530

    Add a text wrapping logic for the long SSID names (WiFi button)

--- .dotfiles/.config/eww/popup_panel/scripts/wifi.sh
+++ .dotfiles/.config/eww/popup_panel/scripts/wifi.sh
@@ -2,6 +2,19 @@
 
 SSID="$(iw wlan0 link| awk '/SSID/ {print $2}')"
 
+# Eww's label word wrapping doesn't seem to work, so truncating the SSID manually
+# At most, place 7 chars on the first lines, followed by 4 on the second
+WRAP_LEN=7
+MAX_LEN=11
+SSID_LEN=$(echo $SSID| wc --chars)
+if [[ $SSID_LEN -gt $WRAP_LEN ]]; then
+    if [[ $SSID_LEN -gt $MAX_LEN ]]; then
+        SSID="${SSID:0:7}\n${SSID:7:4}..."
+    else
+        SSID="${SSID:0:7}\n${SSID:7}"
+    fi
+fi
+
 GETOPTS_OUT=$(getopt -o s -l ssid,toggle -n "wifi.sh" -- "$@")
 [[ $? != 0 ]] && exit 1
 eval set -- "$GETOPTS_OUT"
