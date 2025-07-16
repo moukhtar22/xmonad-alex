7f18c6d2799ed73f29849b52e8d3e87165aa4860
Alex Rosegrid <rosegrid58@gmail.com>
2025-07-16 09:39:37 +0530

    Implement an update button

---
+++ .dotfiles/.config/eww/popup_panel/scripts/update.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+UPDATES_RUNNING=0
+
+if wmctrl -lx| grep -q "PopupPanelUpdateWindow"; then
+    UPDATES_RUNNING=1
+fi
+
+GETOPTS_OUT=$(getopt -o rs -l run,status -n "update.sh" -- "$@")
+[[ $? != 0 ]] && exit 1
+eval set -- "$GETOPTS_OUT"
+
+while true; do
+	case "$1" in
+		-r | --run )
+            alacritty --class "PopupPanelUpdateWindow" -e sudo /usr/local/bin/sync-and-update.sh --ask
+            exit 0 ;;
+        -s | --status )
+            echo "$UPDATES_RUNNING"
+            shift ;;
+        * )
+            shift; break ;;
+	esac
+done
