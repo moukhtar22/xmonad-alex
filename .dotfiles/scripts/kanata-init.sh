4bb5b8ba222309a8b03a094d5cc4ae86640f5464
Alex Rosegrid <rosegrid58@gmail.com>
2025-08-01 09:04:51 +0530

    Add Kanata startup delay
    Fix(probably) the problem with Kanata not setting the key repeat rate
    by introducing a slight delay (0.2s) before `xset r rate`

--- .dotfiles/scripts/kanata-init.sh
+++ .dotfiles/scripts/kanata-init.sh
@@ -4,5 +4,5 @@ if pgrep -x kanata &>/dev/null; then
     killall kanata
 fi
 
-kanata --nodelay --quiet --cfg ~/.config/kanata/kanata-alex.kbd &
 kanata --nodelay --quiet --cfg ~/.config/kanata/kanata-alex.mouse &
+kanata --nodelay --quiet --cfg ~/.config/kanata/kanata-alex.kbd &
